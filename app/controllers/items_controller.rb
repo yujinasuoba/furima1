b6e0d56a60f4a000cc8a379367c60f94a89d1494
yujinasuoba <yuji.nasu.oba@gmail.com>
2020-12-06 22:43:47 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,7 +16,11 @@ class ItemsController < ApplicationController
     else
       render :new
     end
-  end 
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 
   private
   def item_params
