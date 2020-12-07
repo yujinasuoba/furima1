f260ff1b55eb5582eb7c263b7bb9fe7fb9f7e0d1
yujinasuoba <yuji.nasu.oba@gmail.com>
2020-12-08 07:48:49 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new]
+  before_action :authenticate_user!, only:[:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update ]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,7 +20,20 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])  DRY思想によりまとめてset_itemに記述 before_actionで実行させる
+  end
+
+  def edit
+    # @item = Item.find(params[:id])  DRY思想によりまとめてset_itemに記述 before_actionで実行させる
+  end
+
+  def update
+    # @item = Item.find(params[:id])  DRY思想によりまとめてset_itemに記述 before_actionで実行させる
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -28,6 +42,10 @@ class ItemsController < ApplicationController
     :name, :description, :price, :category_id, :condition_id, 
     :shipping_fee_id, :shipping_from_id, :shipping_start_id, 
     :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
